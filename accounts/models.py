ebc4b5950bfab4d18cba6de7feec9f462df6f6e2
restukartiko03 <13516155@std.stei.itb.ac.id>
2019-07-27 14:24:56 +0700

    Add user_detail in profile

--- accounts/models.py
+++ accounts/models.py
@@ -27,17 +27,6 @@ class Profile(models.Model):
         return self.user.username + ' ' + self.user.email
 
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-
-
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-    # instance.profile.save()
-
-
 class TalkHistory(models.Model):
     user1 = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='talk_history1')
